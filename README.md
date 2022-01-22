e38bfff63176f06ec55ed2f444518eb53794f6d5
mnao305 <mail@mnao305.com>
2022-01-23 05:51:21 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# webextension-typescript-template
+
+Browser extension template using Manifest v3, TypeScript, and webpack v5.
+
+## Usage
+
+Press the `Use this template` button to use this template.
+
+### Should be changed at the start of the template
+
+- [ ] project name (package.json, package-lock.json)
+- [ ] project description (package.json)
+- [ ] Each link in package.json
+- [ ] LICENSE
+- [ ] READNE.md
---
+++ webpack.config.ts
@@ -0,0 +1,57 @@
+import path from 'path'
+import { Configuration } from 'webpack'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import { version, name, description } from './package.json'
+
+const config: Configuration = {
+  context: path.join(__dirname, 'src'),
+  entry: {
+    popup: './popup/index.ts',
+    options: './options/index.ts',
+    background: './background/index.ts',
+    content: './content/index.ts'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns:
+      [
+        { from: 'icons', to: 'icons' },
+        { from: 'popup/index.html', to: 'popup/index.html' },
+        { from: 'popup/style.css', to: 'popup/style.css' },
+        { from: 'options/index.html', to: 'options/index.html' },
+        { from: 'options/style.css', to: 'options/style.css' },
+        {
+          from: 'manifest.json',
+          to: 'manifest.json',
+          transform: (content) => {
+            const jsonContent = JSON.parse(content.toString())
+            jsonContent.version = version
+            jsonContent.name = name
+            jsonContent.description = description
+
+            return JSON.stringify(jsonContent, null, 2)
+          }
+        }
+      // { from: '_locales', to: '_locales' } // i18n file
+      ]
+    })
+  ]
+}
+
+export default config
